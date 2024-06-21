092cfd8bf7b6ed497bd48f28ebc82d7d6f503a2c
ilovecxty <70325048+ilovecxty@users.noreply.github.com>
2024-06-21 16:30:28 +0800

    feat: Create gitprojects.sh
    auto download some git projects

---
+++ gitprojects.sh
@@ -0,0 +1,58 @@
+echo "hello git clone"
+
+# awesome
+mkdir awesome
+cd awesome
+git clone https://github.com/sindresorhus/awesome.git
+git clone https://github.com/bayandin/awesome-awesomeness.git
+git clone https://github.com/vinta/awesome-python.git
+git clone https://github.com/fffaraz/awesome-cpp.git
+
+git clone https://github.com/emijrp/awesome-awesome.git
+git clone https://github.com/viatsko/awesome-vscode.git
+git clone https://github.com/tiimgreen/github-cheat-sheet.git
+cd ..
+# awesome shell
+mkdir awesome-shell
+cd awesome-shell
+git clone https://github.com/alebcay/awesome-shell.git
+git clone https://github.com/awesome-lists/awesome-bash.git
+git clone https://github.com/jorgebucaran/awsm.fish.git
+git clone https://github.com/unixorn/awesome-zsh-plugins.git
+
+git clone https://github.com/k4m4/terminals-are-sexy.git
+git clone https://github.com/agarrharr/awesome-cli-apps.git
+
+git clone https://github.com/ibraheemdev/modern-unix.git
+git clone https://github.com/johnalanwoods/maintained-modern-unix.git
+cd ..
+# terminal colorshceme (may have vim)
+mkdir color-term
+cd color-term
+git clone https://github.com/mbadolato/iTerm2-Color-Schemes.git
+git clone https://github.com/chriskempson/tomorrow-theme.git
+git clone https://github.com/sonph/onehalf.git
+cd ..
+# vim
+mkdir color-vim
+cd color-vim
+git clone https://github.com/rafi/awesome-vim-colorschemes.git
+git clone https://github.com/sainnhe/everforest.git
+
+git clone https://github.com/morhetz/gruvbox.git
+git clone https://github.com/altercation/vim-colors-solarized.git
+git clone https://github.com/folke/tokyonight.nvim.git
+git clone https://github.com/catppuccin/nvim.git
+cd ..
+#qt
+mkdir color-qt
+cd color-qt
+git clone https://github.com/Raincode/QtCreator-Color-Schemes.git
+git clone https://github.com/ihonen/qt-creator-themes.git
+cd ..
+#tool
+mkdir tool
+cd tool
+git clone https://github.com/vim/vim.git
+git clone https://github.com/neovim/neovim.git
+cd ..
